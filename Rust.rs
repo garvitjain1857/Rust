a9c290c50d23d321a76cd6f97c6ab08554273f3d
garvit jain <garvitjain1857@gmail.com>
2024-02-07 11:45:28 +0530

    modified:   Rust.rs

--- Rust.rs
+++ Rust.rs
@@ -116,7 +116,7 @@ fn main() {
 }
 
 
-8. Given a binary tree, implement a function that returns the maximum depth of the tree.
+// 8. Given a binary tree, implement a function that returns the maximum depth of the tree.
 
 use std::cmp;
 
